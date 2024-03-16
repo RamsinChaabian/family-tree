34702b9ce08b670607cac47a585920a7d753a7b9
RamsinChaabian <Ramsin.chaabian@gmail.com>
2024-03-16 23:21:32 +0330

    added touchmove event for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var container = document.getElementById('tree');
     
 let scale = 1;
+let initialScale = 1;
+let touchStartDistance = 0;
 
 function handleResize() {
     scale = 0.52;
@@ -34,17 +36,44 @@ function handleWheel(event) {
    
 }
 
+// Listen for touch start event
+document.addEventListener('touchstart', function(event) {
+    if (event.touches.length === 2) {
+        // Calculate the distance between two fingers
+        touchStartDistance = Math.hypot(
+            event.touches[0].clientX - event.touches[1].clientX,
+            event.touches[0].clientY - event.touches[1].clientY
+        );
+        initialScale = scale;
+    }
+});
+
+// Listen for touch move event
+document.addEventListener('touchmove', function(event) {
+    if (event.touches.length === 2) {
+        // Calculate the distance between two fingers during move
+        const touchMoveDistance = Math.hypot(
+            event.touches[0].clientX - event.touches[1].clientX,
+            event.touches[0].clientY - event.touches[1].clientY
+        );
+        
+        // Calculate the new scale based on the initial scale and the ratio of start distance to move distance
+        const newScale = initialScale * (touchMoveDistance / touchStartDistance);
+
+        // Limit the scale factor within desired bounds
+        scale = Math.min(Math.max(0.4, newScale), 3);
+
+        // Apply the new scale
+        container.style.transform = `scale(${scale})`;
+
+        event.preventDefault(); // Prevent default pinch-to-zoom behavior
+    }
+});
+
 handleResize();
 
 window.addEventListener('resize', handleResize);
 container.addEventListener('wheel', handleWheel);
-
-
-
-
-
-
-
 
     var closeIcon = document.querySelector('.close-icon');
     var footer = document.querySelector('footer');
